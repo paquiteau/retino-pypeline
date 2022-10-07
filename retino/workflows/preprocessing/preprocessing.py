8b9cfb4ce6e45ad81ce9247af8ffa641422c3293
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-10-07 18:39:17 +0200

    move builder closer to their call. fix circular dependence.

--- retino/workflows/preprocessing/preprocessing.py
+++ retino/workflows/preprocessing/preprocessing.py
@@ -7,17 +7,17 @@ the denoising methods are provided.
 
 """
 
-from ..base.workflow_manager import WorkflowManager
-from ..base.nodes import file_task
-from ..base.builder import add2sinker, add2wf_dwim
-from ..tools import func2node
-from .builder import (
-    add_coreg,
-    add_denoise_mag,
-    add_denoise_cpx,
-    add_realign,
-    add_topup,
+from retino.workflows.preprocessing.nodes import (
+    cond_denoise_task,
+    conditional_topup_task,
+    coregistration_task,
+    realign_task,
 )
+
+from ..base.builder import add2sinker, add2wf_dwim, add2wf
+from ..base.nodes import file_task
+from ..base.workflow_manager import WorkflowManager
+from ..tools import func2node
 
 _REGEX_SINKER = [
     (r"_sequence_(.*?)([_/])", r"\g<2>"),
@@ -37,6 +37,55 @@ FILES = "selectfiles"
 SINKER = "sink"
 INPUT = "input"
 TEMPLATE = "template_node"
+
+#######################
+#  Builder functions  #
+#######################
+
+
+def add_realign(wf, name, after_node, edge):
+    """Add a Realignment node."""
+    realign = realign_task(name=name)
+    add2wf(wf, after_node, edge, realign, "in_files")
+
+
+def add_denoise_mag(wf, name, after_node, edge):
+    """Add denoising step for magnitude input."""
+    denoise = cond_denoise_task(name)
+    add2wf_dwim(wf, FILES, denoise, ["noise_std_map", "mask"])
+    add2wf_dwim(wf, INPUT, denoise, "denoise_str")
+    add2wf(wf, after_node, edge, denoise, "data")
+
+
+def add_denoise_cpx(wf, name, after_realign=False):
+    """Add denoising step for magnitude input."""
+    denoise = cond_denoise_task(name)
+    add2wf_dwim(wf, FILES, denoise, ["noise_std_map", "mask", "data", "data+_phase"])
+    add2wf_dwim(wf, INPUT, denoise, "denoise_str")
+    if after_realign:
+        add2wf_dwim(wf, REALIGN, denoise, ("realignment_parameters", "motion"))
+
+
+def add_topup(wf, name, after_node, edge):
+    """Add conditional topup correction."""
+    condtopup = conditional_topup_task(name)
+    # also adds mandatory connections
+    add2wf_dwim(wf, INPUT, condtopup, "sequence")
+    add2wf_dwim(FILES, condtopup, "data_opposite")
+    add2wf(wf, after_node, edge, condtopup, "data")
+
+
+def add_coreg(wf, name, after_node, edge):
+    """Add coregistration step."""
+    coreg = coregistration_task(name)
+    # also add mandatory connections:
+    wf.connect(wf.get_node(FILES), "anat", coreg, "in.anat")
+    add2wf(wf, after_node, edge, coreg, "in.func")
+
+
+########################################
+#  Base Preprocessing Worflow  Manager #
+########################################
 
 
 def _tplt_node(sequence, cached_realignment):
@@ -113,6 +162,11 @@ class PreprocessingWorkflowManager(WorkflowManager):
         )
         wf = add2wf_dwim(wf, INPUT, files, templates_args)
         return wf
+
+
+###########################################
+#  Modular Preprocessing Worflow  Manager #
+###########################################
 
 
 class RetinotopyPreprocessingManager(PreprocessingWorkflowManager):
