64eb757fdcc6cbf10a1a5c1219f1d13c43c968cf
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-10-13 10:45:18 +0200

    move preprocessing cli in package, and expose it.

--- retino/cli/preprocessing.py
+++ retino/cli/preprocessing.py
@@ -14,65 +14,69 @@ DATA_DIR = "/neurospin/optimed/pierre-antoine/dataset/data"
 TMP_DIR = "/neurospin/optimed/pierre-antoine/dataset/tmp"
 TASKS = ["Clockwise", "AntiClockwise"]
 SEQUENCES = ["EPI3D"]
-parser = argparse.ArgumentParser(
+
+def get_parser()
+    parser = argparse.ArgumentParser(
     prog="benchmark script for patch denoising method."
     "this script execute a standard retinotopy pipeline with denoising for a set "
     "of subject, but with a single configuration of denoising"
-)
+    )
 
-parser.add_argument(
+    parser.add_argument(
     "--dataset",
     default=DATA_DIR,
     help="location of a BIDS like dataset",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--tmpdir",
     default=TMP_DIR,
     help="location for temp files",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--sequence",
     help="fMRI sequence used",
     nargs="+",
     default=SEQUENCES,
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--denoise-str",
     type=str,
     nargs="+",
     help="denoiser config string",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--build-code",
     type=str,
     nargs="+",
     help="build code for the preprocessing pipeline, eg `Rd`, `rD`, `rd` etc...",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--dry",
     action="store_true",
     help="dry mode:  the workflow is not run, instead exec graph is produced",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--sub",
     type=int,
     nargs="+",
     default=list(range(1, 7)),
     help="list of subject to process.",
-)
-parser.add_argument(
+    )
+    parser.add_argument(
     "--task",
     type=str,
     nargs="+",
     default=TASKS,
     help="task to preprocess",
-)
+    )
+    return parser
 
 
-if __name__ == "__main__":
+def main_cli():
+    parser = get_parser()
+
     ns = parser.parse_args()
     print(ns)
-
     noise_prep_mgr = NoisePreprocManager(ns.dataset, ns.tmpdir)
     noise_prep_wf = noise_prep_mgr.get_workflow()
     noise_prep_mgr.run(
@@ -97,3 +101,7 @@ if __name__ == "__main__":
             sequence=ns.sequence,
             denoise_str=ns.denoise_str,
         )
+
+
+if __name__ == "__main__":
+    main_cli()
