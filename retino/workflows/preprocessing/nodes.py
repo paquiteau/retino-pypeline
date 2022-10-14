b4466b111eab3e20e641738a39075209ab917731
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2022-10-15 01:52:58 +0200

    test mcr again.

--- retino/workflows/preprocessing/nodes.py
+++ retino/workflows/preprocessing/nodes.py
@@ -21,12 +21,12 @@ from retino.interfaces.motion import (
 
 def realign_task(matlab_cmd=None, name="realign"):
     """Create a realign node."""
+    _setup_matlab(None)
     realign = Node(spm.Realign(), name=name)
     realign.inputs.separation = 1.0
     realign.inputs.fwhm = 1.0
     realign.inputs.register_to_mean = False
     realign.n_procs = 3
-    _setup_matlab(realign, matlab_cmd)
     return realign
 
 
@@ -154,9 +154,9 @@ def coregistration_task(name, working_dir=None, matlab_cmd=None):
     roi_coreg = Node(fsl.ExtractROI(t_min=0, t_size=1), name="roi_coreg")
     roi_coreg.inputs.output_type = "NIFTI"
 
+    _setup_matlab(None)
     coreg = Node(spm.Coregister(), name="coregister")
     coreg.inputs.separation = [1, 1]
-    _setup_matlab(coreg, matlab_cmd)
 
     coreg_wf = Workflow(name=name, base_dir=working_dir)
 
