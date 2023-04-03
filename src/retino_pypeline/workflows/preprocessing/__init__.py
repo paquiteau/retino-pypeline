2496bb4e3cf21173a8a8cbd40a57a7585d55e472
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2023-04-03 10:02:28 +0200

    chore: remove old imports.

--- src/retino_pypeline/workflows/preprocessing/__init__.py
+++ src/retino_pypeline/workflows/preprocessing/__init__.py
@@ -1,11 +1,1 @@
 """Preprocessing Workflows."""
-
-from .preprocessing import RetinotopyPreprocessingManager
-from .extras import NoisePreprocManager, RealignmentPreprocessingManager
-
-
-__all__ = [
-    "RealignmentPreprocessingManager",
-    "RetinotopyPreprocessingManager",
-    "NoisePreprocManager",
-]
